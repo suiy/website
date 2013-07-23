ff76cd97804ee7b2d8fdc96cfd5646615ebdeef5
t_suiy <yuan.sui@autodesk.com>
2013-07-23 10:48:13 -0700

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Website::Application.config.secret_token = '8bb18ac1b92fd6a4f363622a3b5ba6de586252a4a6de996bb061cfc910d191583a071e7de9d1508c63015ad0b81dbe97417b2d7fbba001a63fce8cc30434805a'
